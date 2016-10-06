0f1fde39b5f4116375de21e73bec5c56c2a7cf80
Marshall Thompson <marshallthomp@gmail.com>
2016-10-06 15:59:57 -0400

    added octicons to fonts directory

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,7 @@ var $ = require('jquery');
 var _ = require('underscore');
 var Handlebars = require('handlebars');
 var apiKey = require('./githubapikey');
+var octicons = require('octicons');
 
 // set header for api request
 if(apiKey !== undefined){
