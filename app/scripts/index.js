0ef13f6c7c7fedd6073ec15d7873dd4d42b4a2c7
Marshall Thompson <marshallthomp@gmail.com>
2016-10-06 21:49:22 -0400

    avatar data working

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -13,7 +13,35 @@ if(apiKey !== undefined){
 	});
 }
 
+// display
+function displayAvatar(avatar){
+  var $avatars = $('.avatar');
+  $avatars.attr({
+          'src' : avatar.img,
+          'alt' : avatar.name,
+          'title' : avatar.name
+          });
+}
+
+function displayProfileInfo(data){
+  
+}
+
+// recive data and do stuff, call other functions
+function init(data){
+  // grab avatar
+  var avatarData = {
+                     img: data.avatar_url,
+                     name: data.name
+                   };
+
+  displayAvatar(avatarData);
+  // console.log(avatarURL);
+  displayProfileInfo(data);
+}
+
 // ajax request for profile data
 $.ajax('https://api.github.com/users/imarrsh').then(function(data){
-  console.log(data);
+  // call init function
+  init(data);
 });
